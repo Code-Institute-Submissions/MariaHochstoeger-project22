efdae82c5852fde0839b97b837b63793a8e9b327
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2023-10-12 11:28:06 +0000

    Add modal to index.html. Add modal styling to css. Call up modal when How to play button is clicked in JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+// DOM to be loaded before starting game
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+})
+
+let rulesButton = document.getElementById("rules-button");
+let rulesModal = document.getElementById("rules-modal");
+
+rulesButton.onclick = function() {
+    rulesModal.style.display = "block";
+}
+
 function validateUsername() {
 
 }
