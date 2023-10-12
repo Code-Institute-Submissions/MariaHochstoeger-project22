18fd7abb344f62a35bed734b707288b1657b3ffb
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2023-10-12 13:44:16 +0000

    Insert modal text and style modal and modal closing X

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,16 @@ document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 })
 
+// Get Modal to display when "How to Play"-button is clicked
+
 let rulesButton = document.getElementById("rules-button");
 let rulesModal = document.getElementById("rules-modal");
 
 rulesButton.onclick = function() {
     rulesModal.style.display = "block";
 }
+
+
 
 function validateUsername() {
 
