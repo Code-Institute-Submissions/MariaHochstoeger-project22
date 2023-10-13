09b2575097b698ab2e0a9f4b3bf4f1e4b2a33f33
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2023-10-13 14:46:36 +0000

    Clean up code in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,38 @@
-// DOM to be loaded before starting game
-
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
-})
-
-// Modal to display when "How to Play"-button is clicked
-
-let rulesButton = document.getElementById("rules-button");
-let rulesModal = document.getElementById("rules-modal");
-
-rulesButton.onclick = function() {
-    rulesModal.style.display = "block";
-}
-
-// Modal to close when "X" is clicked
-
-let closeModal = document.getElementById("close-modal")
-
-closeModal.onclick = function () {
-    rulesModal.style.display = "none";
-}
-
-// Modal to close when anywhere outside the modal is clicked
-
-window.onclick = function(event) {
-    if (event.target == rulesModal) {
-        rulesModal.style.display = "none";
-    }
-}
-
-// Remove white spaces and validate username
+// Variables in the global space
 
 const usernameField = document.getElementById("input-name");
 const startButton = document.getElementById("lets-start");
 
-/* Disable the Start button 
- credit: https://www.altcademy.com/blog/how-to-disable-a-button-in-javascript/ */
+// DOM to be loaded before starting game
 
-/*startButton.disabled = true;*/
+document.addEventListener("DOMContentLoaded", function() {
+    let rulesButton = document.getElementById("rules-button");
+    let rulesModal = document.getElementById("rules-modal");
+
+    // Modal to display when "How to Play"-button is clicked
+    rulesButton.onclick = function() {
+        rulesModal.style.display = "block";
+    }
+
+    let closeModal = document.getElementById("close-modal")
+
+    // Modal to close when "X" is clicked
+    closeModal.onclick = function () {
+    rulesModal.style.display = "none";
+    }
+
+    // Modal to close when anywhere outside the modal is clicked
+    window.onclick = function(event) {
+        if (event.target == rulesModal) {
+            rulesModal.style.display = "none";
+        }
+    }
+})
 
 // Add event listener to Start Button
 startButton.addEventListener("click", validateUsernameAndStart);
 
+// Validate that username is not blank and redirect to game page 
 function validateUsernameAndStart() {
     // for trim: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim?retiredLocale=de
     if (usernameField.value.trim() != "") {
@@ -50,32 +41,6 @@ function validateUsernameAndStart() {
         alert("Please fill in a name!");
     }
 }
-
-// Function to validate username and enable submit button
-
-
-
-
-
-
-/* Let username = document.getElementById("input-name").value;*/
-
-/*usernameField.onclick = function() {
-    if (username.trim == "") {
-        alert("Please fill out the player name");
-    } else {
-        window.location.href = "game.html";
-    }
-}*/
-
-/*function validateUsername() {
-    if (usernameField.value.trim === "") {
-        alert("Please fill out the player name");
-    } else {
-
-    }
-
-}*/
 
 function runGame() {
 
