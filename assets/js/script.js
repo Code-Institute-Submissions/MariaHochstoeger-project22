0864522d9091102ffb12ec7930b782ef5e6addf4
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2023-10-15 17:04:56 +0000

    Upon selecting, style questions according to correct/not correct, keep score of correct answers. After answering all questions, display score and ask to play again

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,11 +76,13 @@ const questions = [
 ];
 
 const questionElement = document.getElementById("question-field");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
+
+// startQuiz function to reset counts to 0 
 
 function startQuiz() {
     currentQuestionIndex = 0;
@@ -90,16 +92,86 @@ function startQuiz() {
 }
 
 function showQuestion() {
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
+
+    // Display a numbered question
     questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
 
+    // File the answer possibilities into the answerButtons
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButton.appendChild(button);
+        answerButtons.appendChild(button);
+
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
     });
 }
+
+// Remove previous answers / answer fields
+function resetState() {
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+// Add class correct/incorrect to answer buttons to apply styling and increment correct score 
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+
+    /* If selected answer was wrong, highlight the correct answer green by adding .correct class
+    Disable other buttons after answer was chosen */
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+
+    // Display Next button after question is answered
+    nextButton.style.display = "block";
+}
+
+// Show the score, display next button and ask player to play again
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play again";
+    nextButton.style.display = "block";
+}
+
+/* If there are questions remaining, show next question
+Otherwise show the score at the end of the game */
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+};
+
+/* Tell next button what to do
+Either show next Question or start quiz */
+nextButton.addEventListener("click", ()=> {
+    if(currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+})
 
 startQuiz();
